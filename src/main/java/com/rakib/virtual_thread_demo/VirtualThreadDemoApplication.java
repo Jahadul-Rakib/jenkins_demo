8fff4d457a75cd16b5ec37e10277f7db473ecb02
Jahadul-Rakib <rakibdiu2015@gmail.com>
2022-09-18 12:13:44 +0600

    add

---
+++ src/main/java/com/rakib/virtual_thread_demo/VirtualThreadDemoApplication.java
@@ -0,0 +1,18 @@
+package com.rakib.virtual_thread_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualThreadDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run (VirtualThreadDemoApplication.class, args);
+        test();
+    }
+
+    private static void test() {
+
+    }
+
+}
